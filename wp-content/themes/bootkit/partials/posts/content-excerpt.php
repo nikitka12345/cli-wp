9aff530778a1b1cac9b80e59e31e0722e08e6616
Chepenko Diana <chepenkodiana.6040@gmail.com>
2022-01-16 14:30:04 +0200

    cat commit

--- wp-content/themes/bootkit/partials/posts/content-excerpt.php
+++ wp-content/themes/bootkit/partials/posts/content-excerpt.php
@@ -2,12 +2,10 @@
             <div class="card mb-4">
                 <!-- <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap"> -->
                 <?php the_post_thumbnail("full", ["class" => "card-img-top"])?> 
-                <h2 class="card-title"><a href="<?php the_permalink()?>"><?php the_title()?></a></h2>              
+                <h2 class="card-title"><a href="<?php the_permalink()?>"><?php the_title()?></a></h2>  
+                <p>Post category:<?php the_category(" ")?></p>            
                  <div class="card-body">
-                    
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid
-                        atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero
-                        voluptate voluptatibus possimus, veniam magni quis!</p>
+                    <p class="card-text"><?php the_excerpt()?></p>
                     <a href="<?php the_permalink()?>" class="btn btn-primary">Read More &rarr;</a>
                 </div>
                 <div class="card-footer text-muted">
