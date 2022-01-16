365bb6733b87beef506a6b143999804549b8c41e
Chepenko Diana <chepenkodiana.6040@gmail.com>
2022-01-16 13:57:10 +0200

    thum commit

--- wp-content/themes/bootkit/partials/posts/content-excerpt.php
+++ wp-content/themes/bootkit/partials/posts/content-excerpt.php
@@ -1,7 +1,8 @@
  <!-- Blog Post -->
             <div class="card mb-4">
-                <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
-                <div class="card-body">
+                <!-- <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap"> -->
+                <?php the_post_thumbnail("full", ["class" => "card-img-top"])
+ ?>                <div class="card-body">
                     <h2 class="card-title">Post Title</h2>
                     <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid
                         atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero
