800004be1d6d523a98214ef87417db27ba9734b4
Chepenko Diana <chepenkodiana.6040@gmail.com>
2022-01-16 14:21:53 +0200

    date commit

--- wp-content/themes/bootkit/partials/posts/content-excerpt.php
+++ wp-content/themes/bootkit/partials/posts/content-excerpt.php
@@ -1,16 +1,17 @@
  <!-- Blog Post -->
             <div class="card mb-4">
                 <!-- <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap"> -->
-                <?php the_post_thumbnail("full", ["class" => "card-img-top"])
- ?>                <div class="card-body">
-                    <h2 class="card-title">Post Title</h2>
+                <?php the_post_thumbnail("full", ["class" => "card-img-top"])?> 
+                <h2 class="card-title"><a href="<?php the_permalink()?>"><?php the_title()?></a></h2>              
+                 <div class="card-body">
+                    
                     <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid
                         atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero
                         voluptate voluptatibus possimus, veniam magni quis!</p>
-                    <a href="#" class="btn btn-primary">Read More &rarr;</a>
+                    <a href="<?php the_permalink()?>" class="btn btn-primary">Read More &rarr;</a>
                 </div>
                 <div class="card-footer text-muted">
-                    Posted on January 1, 2020 by
-                    <a href="#">Start Bootstrap</a>
-                </div>
+         Posted on <?php echo get_the_date() ?> by
+         <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author()?></a>
+     </div>
             </div> 
