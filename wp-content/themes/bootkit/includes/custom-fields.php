89bfd815519453c7b623acc1626e4797bc860645
Chepenko Diana <chepenkodiana.6040@gmail.com>
2022-01-30 15:02:04 +0200

    chaange commit

---
+++ wp-content/themes/bootkit/includes/custom-fields.php
@@ -0,0 +1,5 @@
+<?php
+function movie_custom_fields()
+{
+    add_post_type_support('movie', 'custom-fields');
+}
--- wp-content/themes/bootkit/includes/custom_fields.php
+++
@@ -1,5 +0,0 @@
-<?php function movie_custom_fields() {
-    add_post_type_support( 'movie', 'custom-fields');
-}
-add_action('init', 'true_custom_fields');
-?>
