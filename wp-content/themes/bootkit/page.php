83036a3099c118616379eeff08646348604103b7
Chepenko Diana <chepenkodiana.6040@gmail.com>
2022-01-30 12:40:56 +0200

    page commit

--- wp-content/themes/bootkit/page.php
+++ wp-content/themes/bootkit/page.php
@@ -1,34 +1,60 @@
 <?php get_header("v2");?>
+
 <!-- Page Content -->
 <div class="container">
     <div class="row">
+
         <!-- Post Content Column -->
-        <div class="col-lg-12">
-            <?php
-while (have_posts()) {
-    the_post();
-    ?>
+        <div class="col-lg-8">
+            <?php if (have_posts()) {
+    while (have_posts()) {
+        the_post();
+        global $post;
+        $author_ID = $post->post_author;
+        $author_URL = get_author_posts_url($author_ID);
+        ?>
             <!-- Title -->
-            <h1 class="mt-4 mb-3"><?php the_title()?></h1>
+            <h1 class="mt-4"><?php the_title()?></h1>
+
+            <!-- Post category: -->
+            <h2 class="mt-4"><?php the_category(" ")?></h2>
+
+            <!-- Author -->
+            <p class="lead">
+                by
+                <a href="<?php echo $author_URL; ?>"><?php the_author();?></a>
+            </p>
             <hr>
+
+            <!-- Date/Time -->
+            <p><?php the_time(get_option('date_format'));
+        echo " ";
+        the_time(get_option('time_format'));?></p>
+            <hr>
+
             <!-- Preview Image -->
-            <!-- <img class="img-fluid rounded" src="http://placehold.it/900x300" alt=""> -->
             <?php
 if (has_post_thumbnail()) {
-        the_post_thumbnail("full", ["class" => "img-fluid rounded"]);
-    }
-    ?>
-            <!-- Page Content -->
-            <?php the_content();
-    ?>
+            the_post_thumbnail("full", ["class" => "card-img-top"]);
+        }
+        ?>
             <hr>
-            <!-- Post Author Info -->
-            <?php
-edit_post_link();
-    ?>
+
+            <!-- Post Content -->
+            <?php the_content();
+        $defaults = array(
+            'before' => '<div class="row justify-content-center align-items-center">' . __('Pages:'),
+            'after' => '</div>',
+        );
+        wp_link_pages($defaults);
+        edit_post_link();
+        ?>
+            <hr>
+
             <!-- Tag cloud -->
             <?php the_tags('', ', ');?>
             <hr>
+
             <!-- Pagination -->
             <ul class="pagination justify-content-center mb-4">
                 <li class="page-item">
@@ -38,18 +64,85 @@ edit_post_link();
                     <?php next_post_link();?>
                 </li>
             </ul>
-            <!-- Comments -->
+
+            <!-- Post Author Info -->
+            <div class="card">
+                <div class="card-header">
+                    <strong>
+                        Posted by
+                        <a href="<?php echo $author_URL; ?>"><?php the_author();?></a>
+                    </strong>
+                </div>
+                <div class="card-body">
+                    <div class="author-image">
+                        <?php echo get_avatar($author_ID, 90, '', false, ['class' => 'img-circle']); ?>
+                    </div>
+                    <?php echo nl2br(get_the_author_meta('description')); ?>
+                </div>
+            </div><!-- Post Single - Author End -->
+
+            <h4>Related Posts:</h4>
+            <div class="related-posts clearfix">
+                <?php
+$categories = get_the_category();
+        $rp_query = new WP_Query([
+            'posts_per_page' => 2,
+            'post__not_in' => [$post->ID],
+            'cat' => !empty($categories) ? $categories[0]->term_id : null,
+        ]);
+        if ($rp_query->have_posts()) {
+            while ($rp_query->have_posts()) {
+                $rp_query->the_post();
+                ?>
+                <div class="mpost clearfix">
+                    <?php
+if (has_post_thumbnail()) {
+                    ?>
+                    <div class="entry-image">
+                        <a href="<?php the_permalink();?>">
+                            <?php the_post_thumbnail('thumbnail');?>
+                        </a>
+                    </div>
+                    <?php
+}
+                ?>
+                    <div class="entry-c">
+                        <div class="entry-title">
+                            <h4>
+                                <a href="<?php the_permalink();?>">
+                                    <?php the_title();?>
+                                </a>
+                            </h4>
+                        </div>
+                        <ul class="entry-meta clearfix">
+                            <li><?php echo get_the_date(); ?></li>
+                            <li><?php comments_number('0');?></li>
+                        </ul>
+                        <div class="entry-content">
+                            <?php the_excerpt();?>
+                        </div>
+                    </div>
+                </div>
+                <?php
+}
+            wp_reset_postdata();
+        }
+        ?>
+            </div>
             <?php
-    if (comments_open() || get_comments_number()) {
-        comments_template();
-    }
-    ?>
+if (comments_open() || get_comments_number()) {
+            comments_template();
+        }
+        ?>
             <?php
 }
-?>
+}?>
         </div>
+        <?php get_sidebar();?>
         <!-- /.row -->
+
     </div>
     <!-- /.container -->
+
 </div>
 <?php get_footer();?>
