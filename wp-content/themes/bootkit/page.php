6c40e7e11013c9869573ecff52b9ec05c81e57ce
Chepenko Diana <chepenkodiana.6040@gmail.com>
2022-01-30 12:46:23 +0200

    change page commit

--- wp-content/themes/bootkit/page.php
+++ wp-content/themes/bootkit/page.php
@@ -55,15 +55,7 @@ if (has_post_thumbnail()) {
             <?php the_tags('', ', ');?>
             <hr>
 
-            <!-- Pagination -->
-            <ul class="pagination justify-content-center mb-4">
-                <li class="page-item">
-                    <?php previous_post_link();?>
-                </li>
-                <li class="page-item">
-                    <?php next_post_link();?>
-                </li>
-            </ul>
+            
 
             <!-- Post Author Info -->
             <div class="card">
@@ -81,54 +73,7 @@ if (has_post_thumbnail()) {
                 </div>
             </div><!-- Post Single - Author End -->
 
-            <h4>Related Posts:</h4>
-            <div class="related-posts clearfix">
-                <?php
-$categories = get_the_category();
-        $rp_query = new WP_Query([
-            'posts_per_page' => 2,
-            'post__not_in' => [$post->ID],
-            'cat' => !empty($categories) ? $categories[0]->term_id : null,
-        ]);
-        if ($rp_query->have_posts()) {
-            while ($rp_query->have_posts()) {
-                $rp_query->the_post();
-                ?>
-                <div class="mpost clearfix">
-                    <?php
-if (has_post_thumbnail()) {
-                    ?>
-                    <div class="entry-image">
-                        <a href="<?php the_permalink();?>">
-                            <?php the_post_thumbnail('thumbnail');?>
-                        </a>
-                    </div>
-                    <?php
-}
-                ?>
-                    <div class="entry-c">
-                        <div class="entry-title">
-                            <h4>
-                                <a href="<?php the_permalink();?>">
-                                    <?php the_title();?>
-                                </a>
-                            </h4>
-                        </div>
-                        <ul class="entry-meta clearfix">
-                            <li><?php echo get_the_date(); ?></li>
-                            <li><?php comments_number('0');?></li>
-                        </ul>
-                        <div class="entry-content">
-                            <?php the_excerpt();?>
-                        </div>
-                    </div>
-                </div>
-                <?php
-}
-            wp_reset_postdata();
-        }
-        ?>
-            </div>
+     
             <?php
 if (comments_open() || get_comments_number()) {
             comments_template();
