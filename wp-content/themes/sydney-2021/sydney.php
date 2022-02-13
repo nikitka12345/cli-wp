ba810e3aee48e89c24f88096d913eb800a7a1a7b
Chepenko Diana <chepenkodiana.6040@gmail.com>
2022-02-13 14:50:31 +0200

    content-front add

--- wp-content/themes/sydney-2021/sydney.php
+++ wp-content/themes/sydney-2021/sydney.php
@@ -2,9 +2,51 @@
 // Template Name: Sydney 2021
 
 get_header();
-
-while (have_posts()): the_post();
-    the_content();
+ 
+?>
+ 
+<main id="primary" class="site-main">
+    <section class="articleSpaceBlock container">
+        <h4 class="tittleBlock padleft"><?php bloginfo('name');?></h4>
+        <?php
+// The Query
+$the_query = new WP_Query(array(
+    'post_type' => 'post', 'category_name' => 'news'));
+if ($the_query->have_posts()):
+ 
+    if (is_home() && !is_front_page()):
+    ?>
+        <header>
+            <h1 class="page-title screen-reader-text"><?php single_post_title();?></h1>
+        </header>
+        <?php
+endif;
+ 
+/* Start the Loop */
+while ($the_query->have_posts()):
+    $the_query->the_post();
+ 
+    /*
+     * Include the Post-Type-specific template for the content.
+     * If you want to override this in a child theme, then include a file
+     * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+     */
+    get_template_part('template-parts/content', "front");
+ 
 endwhile;
-
+ 
+the_posts_navigation();
+ 
+else:
+ 
+    get_template_part('template-parts/content', 'none');
+ 
+endif;
+wp_reset_postdata();
+?>
+    </section>
+ 
+</main><!-- #main -->
+ 
+<?php
 get_footer();
---
+++ wp-content/themes/sydney-2021/template-parts/content-front.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Sydney_2021
+ */
+ 
+?>
+ 
+<div class="articleSpace">
+    <div class="imgBlock">
+        <?php sydney_2021_post_thumbnail();?>
+    </div>
+    <div class="contentText">
+        <h5 class="titleArticle"><?php the_title();?></h5>
+        <?php
+ 
+the_content(
+    sprintf(
+        ('MORE INFO'),
+        array(
+            'a' => array(
+                'class' => array("moreInfo"),
+            ),
+        )
+    )
+ 
+);
+?>
+        <p> <a href="#" class="bookNow">BOOK NOW</a></p>
+    </div><!-- .entry-content -->
+</div><!-- #post-<?php the_ID();?> -->
